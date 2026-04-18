b2087421460a091d00253882dec898981bce8f9a
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-18 15:07:38 +0300

    drop ide api deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,17 @@
  * @since 15/01/2023
  */
 module org.apache.pig {
-  requires consulo.ide.api;
+  requires consulo.application.api;
+  requires consulo.code.editor.api;
+  requires consulo.color.scheme.api;
+  requires consulo.document.api;
+  requires consulo.language.api;
+  requires consulo.language.editor.api;
+  requires consulo.language.impl;
+  requires consulo.localize.api;
+  requires consulo.project.api;
+  requires consulo.ui.api;
+  requires consulo.virtual.file.system.api;
 
   exports consulo.apache.pig.icon;
   exports org.intellij.pig;
